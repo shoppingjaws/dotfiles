1cd1091a85199ff1c8b577415322a9b38de9cb47
ShoppingJaws <25874783+nakamuloud@users.noreply.github.com>
2023-11-24 22:23:52 +0900

    update

---
+++ dot_config/install.sh
@@ -0,0 +1,8 @@
+# install zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+# setup git
+git config --global user.email "25874783+nakamuloud@users.noreply.github.com"
+
+sudo softwareupdate --install-rosetta
+brew install ghq gh starship zplug awscli chezmoi
+brew install --cask iterm2 google-japanese-ime google-cloud-sdk slack arc raycast alt-tab 1password
