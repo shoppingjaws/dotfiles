5904d451ef98847746e360946f211deb88c6f78f
ShoppingJaws <25874783+nakamuloud@users.noreply.github.com>
2023-11-24 22:40:52 +0900

    update

--- dot_config/install.sh
+++ dot_config/install.sh
@@ -6,3 +6,7 @@ git config --global user.email "25874783+nakamuloud@users.noreply.github.com"
 sudo softwareupdate --install-rosetta
 brew install ghq gh starship zplug awscli chezmoi
 brew install --cask iterm2 google-japanese-ime google-cloud-sdk slack arc raycast alt-tab 1password
+
+# install font
+curl -o ~/Downloads/font.ttf https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/SauceCodeProNerdFontMono-Regular.ttf
+open ~/Downloads/font.ttf
