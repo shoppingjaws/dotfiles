86e9114d3a6f82f654792e7981c4679cd614a2ab
ShoppingJaws <25874783+nakamuloud@users.noreply.github.com>
2023-12-09 21:35:04 +0900

    update

--- dot_config/install.sh
+++ dot_config/install.sh
@@ -4,9 +4,14 @@ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/instal
 git config --global user.email "25874783+nakamuloud@users.noreply.github.com"
 
 sudo softwareupdate --install-rosetta
-brew install ghq gh starship zplug awscli chezmoi
+brew install ghq gh starship zplug awscli chezmoi lazygit fzf zsh-completions fig
 brew install --cask iterm2 google-japanese-ime google-cloud-sdk slack arc raycast alt-tab 1password
 
 # install font
 curl -o ~/Downloads/font.ttf https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/SauceCodeProNerdFontMono-Regular.ttf
 open ~/Downloads/font.ttf
+
+# setup zsh completion
+# works on Apple sillicon
+chmod go-w '/opt/homebrew/share'
+chmod -R go-w '/opt/homebrew/share/zsh'
