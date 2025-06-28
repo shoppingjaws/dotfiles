b99bd34ef93a532237b06bcf78c38ba52acea182
ShoppingJaws <25874783+shoppingjaws@users.noreply.github.com>
2025-06-28 22:47:01 +0900

    fix(setup.sh): correct OCO_GIT_PUSH config key to OCO_GITPUSH

--- npm/setup.sh
+++ npm/setup.sh
@@ -3,5 +3,5 @@ npm install -g opencommit
 oco config set OCO_MODEL=claude-3-5-sonnet-20240620
 oco config set OCO_AI_PROVIDER=anthropic
 oco config set OCO_LANGUAGE=jp
-oco config set OCO_GIT_PUSH=false
+oco config set OCO_GITPUSH=false
 # oco config set OCO_API_KEY $ANTHROPIC_API_KEY
